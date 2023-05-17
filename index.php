95ced7ed7664991928284ec5a1288634715ccae0
ekoaprianda <133642518+ekoaprianda@users.noreply.github.com>
2023-05-17 11:01:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="libs/css/bootstrap.v3.3.6.css">
-	<script type="text/javascript" src="assets/signature.js"></script>
+	
 
 
 
